abeabcf3a61bcf46df1ccf03e30403b37a496fd7
Mattis Springholz <mattis.sp@web.de>
2024-11-04 23:11:23 +0100

    Initial commit

---
+++ Lexware_Task/src/main/java/project/Lexware_Task/controller/Controller.java
@@ -0,0 +1,14 @@
+package project.Lexware_Task.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Spring Boot!";
+    }
+}
---
+++ Lexware_Task/src/test/java/project/Lexware_Task/LexwareTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package project.Lexware_Task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexwareTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
